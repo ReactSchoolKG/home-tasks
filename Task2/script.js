083b92e98e46537fd2df06a82bdc8c485d5150bb
Vasyljev Mykyta <vasyljev.mykyta@gmail.com>
2018-11-03 15:56:47 +0200

    Task2 v 2.0.0

--- Task2/script.js
+++ Task2/script.js
@@ -1,18 +1,30 @@
+let bodyBlock = document.getElementsByTagName('body');
 let divBlock = document.getElementsByTagName('div');
 let pBlock = document.getElementsByTagName('p');
 let buttonBlock = document.getElementsByTagName('button');
+
+bodyBlock[0].addEventListener('click', alertMessage, false);
 divBlock[0].addEventListener('click', getInfoOne, true);
 divBlock[0].addEventListener('click', getInfoTwo, false);
 pBlock[0].addEventListener('click', getInfoOne, true);
 pBlock[0].addEventListener('click', getInfoTwo, false);
 buttonBlock[0].addEventListener('click', getInfoOne, true);
 buttonBlock[0].addEventListener('click', getInfoTwo, false);
+
 function getInfoOne() {
-    eventTarget = event.target;
-    console.log(`This is Capture! Event target: ${eventTarget}, this: ${this}`);
+    let eventTarget = event.target;
+    console.log(`This is Capture! Event target: ${eventTarget.getAttribute('id')}, this: ${this.getAttribute('id')}`);
 }
 
 function getInfoTwo() {
-    eventTarget = event.target;
-    console.log(`This is Bubbling! Event target: ${eventTarget}, this: ${this}`);
+    let eventTarget = event.target;
+    if(this == divBlock[0]) {
+        event.stopPropagation();
+    }
+    console.log(`This is Bubbling! Event target: ${eventTarget.getAttribute('id')}, this: ${this.getAttribute('id')}`);
+}
+
+function alertMessage() {
+    
+    alert('Event on body!');
 }
