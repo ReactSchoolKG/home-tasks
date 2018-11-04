f7d68b10a4c890efce8b107e25bffb4bf788741a
KeinOhrHasen <34781917+KeinOhrHasen@users.noreply.github.com>
2018-11-05 00:48:51 +0200

    update script.js
    improve code look

--- task_2/script.js
+++ task_2/script.js
@@ -1,15 +1,15 @@
-var elems = document.querySelectorAll('div, p, button');
+let elems = document.querySelectorAll('div, p, button');
 let body = document.querySelector("body");
 let div = document.querySelector("div");
 
-for (var i = 0; i < elems.length; i++) {
+for (let i = 0; i < elems.length; i++) {
   elems[i].addEventListener("click", highlightThis, true);
   elems[i].addEventListener("click", highlightThis, false);
 }
 
 body.addEventListener("click", ()=>{alert("Event on body")}, false);
-div.addEventListener("click", (e)=>e.stopImmediatePropagation());
+div.addEventListener("click", (event)=> event.stopPropagation());
 
-function highlightThis(e){
-    alert(`event on: ${this.tagName}, current target: ${e.target.tagName}`)
+function highlightThis(event){
+    alert(`event on: ${this.tagName}, current target: ${event.target.tagName}`)
 }
