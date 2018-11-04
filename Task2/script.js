e20b60f9741585a694f9c7d4b75240cfedb0a4aa
DyakivYurii <dyakivyurii@gmail.com>
2018-11-04 12:02:58 +0200

    split function to bind and showMes

--- Task2/script.js
+++ Task2/script.js
@@ -3,24 +3,34 @@ const pList = [...document.querySelectorAll(`.item__text`)];
 const buttonList = [...document.querySelectorAll(`.item__button`)];
 
 /**
+ * For show message on the console
+ * @param {Boolean} stopPropagation - use for stop propagation
+ * @param {String} color - text color on the console
+ */
+const showMessage = (stopPropagation = false, color = `black`, event) => {
+    if(stopPropagation) {
+        event.stopPropagation();
+    }
+    console.log('%cBubbling phase', `color: ${color}; font-weight: bold;`);
+    console.log(`Current element (target) =`, event.target);
+    console.log(`Element with handler (this) =`, event.currentTarget);
+};
+
+/**
  * Bind for all elements eventListener
  * @param {Array} elements - this is array of elements
  * @param {Boolea} stopPropagation - if true then put stopPropagation 
+ * @param {Object} event
  */
 const bind = (elements, stopPropagation = false) => {
     elements.forEach((element) => {
         element.addEventListener(`click`, function(event) {
-            console.log(`%cCapture phase`, `color: indigo; font-weight: bold;`);
-            console.log(`Current element (target) =`, event.target);
-            console.log(`Element with handler (this) =`, event.currentTarget);
+            setTimeout(showMessage(false, `indigo`, event), 0);
         }, true);
         element.addEventListener(`click`, function(event) {
-            if(stopPropagation) {
-                event.stopPropagation();
-            }
-            console.log('%cBubbling phase', `color: green; font-weight: bold;`);
-            console.log(`Current element (target) =`, event.target);
-            console.log(`Element with handler (this) =`, event.currentTarget);
+            !stopPropagation 
+                ? setTimeout(showMessage(false, `green`, event), 0) 
+                : setTimeout(showMessage(true, `green`, event), 0);
         });
     }); 
 };
