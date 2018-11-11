4dba9c39dbeaef94aac475cdafab10bf32b0497b
dima <nevdaha505@gmail.com>
2018-11-12 00:43:23 +0200

    reworked local storage : merge items in array

--- litvinchuk-dmytro-Task3/Task3/itemsHelper.js
+++ litvinchuk-dmytro-Task3/Task3/itemsHelper.js
@@ -1,57 +1,48 @@
 const itemsHelper = {
   list : document.getElementById("list"),
 
-  items : [],
+  items : JSON.parse(localStorage.getItem("items")) || [],
 
   fillWithItems : function(){
     this.items.push({
       title :'react',
-      done : false
+      done : false,
+      id : this.items.length+1
     });
     this.items.push({
       title :'angular',
-      done : false
+      done : false,
+      id : this.items.length+1
     });
     this.items.push({
       title :'pure js',
-      done : false
+      done : false,
+      id : this.items.length+1
     });
-    let length = localStorage.length+3;
-    for(let i = localStorage.length+1, j = this.items.length - 2; i <= length; i++, j++){
-      this.items[j-1].id = i;
-      localStorage.setItem('listEl' + JSON.stringify(i), JSON.stringify(this.items[j-1]));
-    }
+    localStorage.setItem("items", JSON.stringify(this.items));
     this.repaint();
   },
 
   addItem : function(title){
     let item = {};
-    localStorage.length === 0 ?
+    this.items.length === 0 ?
       item = {title: title, done: false, id : 1} :
-      item = {title: title, done: false, id : localStorage.length+1};
-    localStorage.setItem('listEl' + JSON.stringify(item.id), JSON.stringify(item));
+      item = {title: title, done: false, id : this.items.length+1};
     this.items.push(item);
+    localStorage.setItem("items", JSON.stringify(this.items));
     this.repaint();
   },
 
   removeItem: function(e,index) {
     e.stopPropagation();
     this.items.splice(index-1,1);
-    let counter = 1;
-    if(index !== localStorage.length){
-      for (let i = 1; i <= localStorage.length; i++) {
-        if (i !== index) {
-          let item = JSON.parse(localStorage.getItem('listEl' + JSON.stringify(i)));
-          item.id = counter;
-          localStorage.setItem('listEl' + JSON.stringify(item.id), JSON.stringify(item));
-          counter++;
-        }
-      }
-      localStorage.removeItem('listEl' + JSON.stringify(localStorage.length));
+    let counter = index;
+    for (let i = index; i <= this.items.length; i++) {
+        let item = this.items[i-1];
+        item.id = counter;
+        counter++;
     }
-    else{
-      localStorage.removeItem('listEl' + JSON.stringify(localStorage.length));
-    }
+    localStorage.setItem("items", JSON.stringify(this.items));
     this.repaint();
   },
 
@@ -59,27 +50,29 @@ const itemsHelper = {
     let fragment = document.createDocumentFragment();
     this.list.innerHTML = "";
     let i = 0;
-    while(i < localStorage.length){
-      let item = JSON.parse(localStorage.getItem('listEl' + JSON.stringify(i+1)));
+    while (i < this.items.length) {
+      let item = this.items[i];
       let self = this;
       let li = document.createElement('li');
       li.innerHTML = `${item.title} <button class="deleteButtons" onclick="itemsHelper.removeItem(event, ${item.id})">x</button>`;
-      li.addEventListener('click',function(){
+      li.addEventListener('click', function () {
         self.item = item;
-        (function() {
+        (function () {
           event.stopPropagation();
-          self.toggle(li,self.item.id);
+          self.toggle(li, self.item.id);
         })();
       });
-      if(item.done === true){li.style.textDecoration = "line-through"}
+      if (item.done === true) {
+        li.style.textDecoration = "line-through"
+      }
       fragment.appendChild(li);
       i++;
     }
     this.list.appendChild(fragment);
-    if(itemsHelper.list.children.length > 0){
+    if (itemsHelper.list.children.length > 0) {
       itemsHelper.list.style.visibility = "visible";
     }
-    else{
+    else {
       itemsHelper.list.style.visibility = "hidden";
     }
   },
@@ -91,12 +84,12 @@ const itemsHelper = {
   },
 
   toggle(el,id){
-    let item = JSON.parse(localStorage.getItem('listEl' + JSON.stringify(id)));
+    let item = this.items[id-1];
     item.done = !item.done;
     item.done === false ?
       el.style.textDecoration = "none"
       : el.style.textDecoration = "line-through";
-    localStorage.setItem('listEl' + JSON.stringify(item.id), JSON.stringify(item));
+    localStorage.setItem("items", JSON.stringify(this.items));
   }
 
 };
