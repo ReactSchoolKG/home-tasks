3fb1ebfa1b614fcc4f68c689d33716a2e944667a
inkludo <41345108+inkludo@users.noreply.github.com>
2018-11-15 01:24:31 +0200

    Delete main.js

--- Task3-lytvynenko-vladyslav/js/main.js
+++
@@ -1,8 +0,0 @@
-function readyDOM() {
-    console.log('DOM loaded');
-    let items = JSON.parse(localStorage.getItem('items')) || [];
-    itemsHelper.setItems(items);
-  }
-  
-  document.addEventListener('DOMContentLoaded', readyDOM);
-  
